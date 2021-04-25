d1ede8d48e13cffc1f48cee0f6941f62fb5ee0d2
Abhisheknayak-programer <sahilservice300@gmail.com>
2021-04-25 12:17:16 +0530

    Link added

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Footwear",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true}).then(()=>{
+mongoose.connect("mongodb+srv://hubspot:welcome2020@cluster0.wq487.mongodb.net/footware",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true}).then(()=>{
     console.log("We Are Connected guys");
 }).catch((err)=>{
     console.log(err);
 })
 
 // mongodb+srv://hubspot:welcome2020@cluster0.wq487.mongodb.net/footware
+// mongodb://localhost:27017/Footwear
